01d015fdcd67e9b73774360cf796e6bbda770e24
James Ho <jamessho821@gmail.com>
2025-01-27 16:09:34 -0800

    added commands for clearing cache

--- app.js
+++ app.js
@@ -2,21 +2,42 @@ require("dotenv").config();
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const ProxyServer = require('./proxy-server')
+const cacheService = require("./cache-service");
 
-const argv = yargs(hideBin(process.argv))
-  .option("port", {
-    alias: "p",
-    type: "number",
-    description: "Port to run the server on",
-    default: 3000,
-  })
-  .option("origin", {
-    alias: "o",
-    type: "string",
-    description: "Origin URL to viist",
-    default: "",
-  }).argv;
-
-console.log(argv.port, argv.origin)
-const server = new ProxyServer(argv.origin, argv.port)
-server.start()
+yargs(hideBin(process.argv))
+    .command(
+        "*",// Command name and optional argument
+        "Greet a user by name", // Command description
+        (yargs) => {
+            return yargs
+            .positional("port", {
+                describe: "Name of the person to greet",
+                default: "3000",
+            }).positional("origin", {
+                describe: "Name of the person to greet",
+            });
+        },
+        (argv) => {
+            // Command handler
+            const server = new ProxyServer(argv.origin, argv.port)
+            server.start()
+        }
+    ).command(
+        "clear",
+        "Greet a user by name", // Command description
+        (yargs) => {
+            return yargs.positional("clear-cache", {
+                describe: "Clears the cache",
+            });
+        },
+        async() => {
+            // Command handler
+            try {
+                console.log('flush cache');
+                await cacheService.FLUSHDB()
+            } catch(err) {
+                console.error(err)
+            }
+        }
+    )
+    .argv;
